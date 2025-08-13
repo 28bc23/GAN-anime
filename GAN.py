ea3bf73b1d8911dffb0a918e579f99f7929728be
JondaMC <jonas.fris@seznam.cz>
2025-08-13 18:27:09 +0200

    Generator Sequential net and forward function

--- GAN.py
+++ GAN.py
@@ -5,7 +5,27 @@ import torch.optim as optim
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
-        self.main = nn.Sequential()
+        self.main = nn.Sequential(
+            nn.Linear(1024 * 512, 128 * 256 * 128),
+            nn.ReLU(),
+            nn.Unflatten(1,(128, 256, 128)),
+
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+
+            nn.Conv2d(64, 3, kernel_size=3, padding=1),
+            nn.Tanh() #Based on transform.Compose
+        )
+    def forward(self, input):
+        img = self.main(input)
+        return img
 
 class Discriminator(nn.Module):
     def __init__(self):
