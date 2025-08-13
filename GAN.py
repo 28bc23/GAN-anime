04135750dbaecb02aa472c3dcb5db08c03b49138
JondaMC <jonas.fris@seznam.cz>
2025-08-13 18:29:32 +0200

    device var add

--- GAN.py
+++ GAN.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
